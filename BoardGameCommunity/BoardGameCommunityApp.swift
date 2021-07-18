023f59c20d0f0d7df6a4b412acc735bc67fccfc5
JimCampagno <jimcampagno@gmail.com>
2021-07-18 15:55:27 -0400

    Add Xcode project

---
+++ BoardGameCommunity/BoardGameCommunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameCommunityApp.swift
+//  BoardGameCommunity
+//
+//  Created by Jim Campagno on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameCommunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
